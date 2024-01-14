ed7accb3acc98b089c1f6bda4488b62ad7041dc9
Boytsov Dmitry <bdo5677905@yandex.ru>
2024-01-14 12:26:25 +0300

    first

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -2,6 +2,37 @@
  * Основная функция для совершения запросов
  * на сервер.
  * */
-const createRequest = (options = {}) => {
 
+const createRequest = (options = {}) => {
+  const xhr = new XMLHttpRequest();
+  xhr.responseType = 'json';
+
+  let generalURL = options.url;
+
+  if(options.method === 'GET') {
+    generalURL = generalURL + '?mail=' + options.data.mail + '&password=' + options.data.password;
+    xhr.open(options.method, generalURL);
+    xhr.send();
+  } else {
+    formData = new FormData();
+
+    for (key in options.data) {
+      formData.append(key, options.data[key]);
+    };
+
+    xhr.open(options.method, generalURL);
+    xhr.send(formData);
+  };
 };
+
+createRequest({
+  url: 'user',
+  data: {
+    mail: 'ivan@poselor.ru',
+    password: 'odinodin'
+  },
+  method: 'POST',
+  callback: (err,response) => {
+    console.log(err,response)
+  }
+})
